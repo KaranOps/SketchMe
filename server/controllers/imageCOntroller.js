a1bd26f8046122087208b02b8dcf18d117b112e9
Karankumar2004 <kk8594879@gmail.com>
2025-05-13 20:33:15 +0530

    Securtiy

--- server/controllers/imageCOntroller.js
+++ server/controllers/imageCOntroller.js
@@ -14,10 +14,14 @@ export const generateImage = async (req, res) => {
         if (userModel.creditBalance === 0 || userModel.creditBalance < 0) {
             res.json({ success: false, message: "No credit balance", creditBalance: user.creditBalance })
         }
-
+        
+        //1. Create a new form to send data
         const formData = new FormData()
+        // 2. Add the prompt to the form
         fromData.append('prompt', prompt)
+        
 
+        // 3. Make a POST request to ClipDrop API
         const { data } = await axios.post('https://clipdrop-api.co/cleanup/v1', formData, {
             headers: {
                 'x-api-key': CLIPDROP_API,
@@ -25,13 +29,18 @@ export const generateImage = async (req, res) => {
             responseType: 'arraybuffer',
         })
 
+        // 4. Convert binary image to base64
         const base64Image = Buffer.from(data,'binary').toString('base64')
+
+        // 5. Prepare the final image for the browser
         const resultImage = `data:image/png;base64,${base64Image}`
 
+        // 6. Update user's credit balance
         await userModel.findByIdAndUpdate(user._id, {
             creditBalance: user.creditBalance - 1
         })
-
+        
+        // 7. Send the result back to the frontend    
         res.json({success:true, message: "Image Generated", creditBalance: user.creditBalance-1, resultImage})
     } catch (error) {
 

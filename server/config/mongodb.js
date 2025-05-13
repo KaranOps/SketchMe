39e7ad222c61276b35a43ecb38ae4132ad9f0e53
Karankumar2004 <kk8594879@gmail.com>
2025-05-13 20:22:04 +0530

    Done till Clipdrop api creation

---
+++ server/config/mongodb.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const connectDB = async ()=>{
+    mongoose.connection.on('connected', () => {
+        console.log("Database Connected");
+    })
+    try {
+    await mongoose.connect(process.env.MONGODB_URI);
+  } catch (err) {
+    console.error('Mongo connection error:', err.message);
+    process.exit(1);
+  }
+}
+
+export default connectDB;
---
+++ server/middlewares/auth.js
@@ -0,0 +1,24 @@
+import jwt from 'jsonwebtoken'
+
+const userAuth = (req, res, next) => {
+    const {token} = req.headers;
+
+    if(!token) {
+        return res.json({success: false, message: "Unauthorized, login again"})
+    }
+
+    try {
+        const tokenDecode = jwt.verify(token, process.env.JWT_SECRET);
+
+        if(tokenDecode.id) {
+            req.body.userId = tokenDecode.id;
+        }else{
+            return res.json({success: false, message: "Unauthorized, login again"})
+        }
+        next();
+    } catch (error) {
+        res.json({success:false, message: error.message})
+    }
+};
+
+export default userAuth;

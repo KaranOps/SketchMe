474244d1ccf114b5d98def5bae778fa7365d00a0
Karankumar2004 <kk8594879@gmail.com>
2025-05-14 23:24:46 +0530

    Backend Complete Login-SignUp Done

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -2,10 +2,53 @@ import React, { useContext, useEffect, useState } from 'react'
 import { assets } from '../assets/assets'
 import { AppContext } from '../context/AppContext'
 import { delay, motion } from "motion/react"
+import axios from 'axios'
+import { toast } from 'react-toastify'
 
 const Login = () => {
     const [state, setState] = useState('Login')
-    const { setShowLogin } = useContext(AppContext)
+    const { setShowLogin, backendUrl, setToken, setUser } = useContext(AppContext)
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const onSubmitHandler = async (e) => {
+        e.preventDefault();
+
+        try {
+
+            if (state === 'Login') {
+                const response = await axios.post(backendUrl + '/api/user/login', { email, password })
+                const data = response.data;
+                if (data.success) {
+                    setToken(data.token)
+                    setUser(data.user)
+                    localStorage.setItem('token', data.token)
+                    setShowLogin(false)
+                } else {
+                    toast.error(data.message)
+                }
+            } else {
+                const response = await axios.post(backendUrl + '/api/user/register', { name, email, password })
+                const data = response.data;
+                if (data.success) {
+                    setToken(data.token)
+                    setUser(data.user)
+                    localStorage.setItem('token', data.token)
+                    setShowLogin(false)
+                } else {
+                    toast.error(data.message)
+                }
+            }
+        } catch (error) {
+            const message =
+                error?.response?.data?.message ||   // from backend
+                error?.message ||                   // from Axios/network
+                "Something went wrong. Please try again.";
+
+            toast.error(message);
+        }
+    }
 
     useEffect(() => {
         document.body.style.overflow = 'hidden'
@@ -15,7 +58,7 @@ const Login = () => {
     })
     return (
         <div className='fixed top-0 left-0 bottom-0 right-0 z-10 backdrop-blur-sm bg-black/30 flex justify-center items-center'>
-            <motion.form
+            <motion.form onSubmit={onSubmitHandler}
                 initial={{ opacity: 0.2, y: 50 }}
                 transition={{ duration: 0.3 }}
                 whileInView={{ opacity: 1, y: 0 }}
@@ -26,17 +69,17 @@ const Login = () => {
 
                 {state !== 'Login' && <div className='border px-6 py-2 flex items-center gap-2 rounded-full mt-5'>
                     <img src={assets.user_icon} alt="" />
-                    <input type="text" placeholder='Full Name' required className='outline-none text-sm' />
+                    <input onChange={e => setName(e.target.value)} value={name} type="text" placeholder='Full Name' required className='outline-none text-sm' />
                 </div>}
 
                 <div className='border px-6 py-2 flex items-center gap-2 rounded-full mt-4'>
                     <img src={assets.email_icon} alt="" />
-                    <input type="email" placeholder='Email Id' required className='outline-none text-sm' />
+                    <input onChange={e => setEmail(e.target.value)} value={email} type="email" placeholder='Email Id' required className='outline-none text-sm' />
                 </div>
 
                 <div className='border px-6 py-2 flex items-center gap-2 rounded-full mt-4'>
                     <img src={assets.lock_icon} alt="" />
-                    <input type="password" placeholder='Password' required className='outline-none text-sm' />
+                    <input onChange={e => setPassword(e.target.value)} value={password} type="password" placeholder='Password' required className='outline-none text-sm' />
                 </div>
 
                 <p className='text-sm text-blue-600 my-4 cursor-pointer'>Forgot Password?</p>
